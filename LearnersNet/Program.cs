e1fe13457c751f7dcaba5fd32603378ce1038d28
pythagoras58 <solomondanso58@gmail.com>
2022-11-16 20:59:47 +0000

    Add project files.

---
+++ LearnersNet/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LearnersNet
+{
+    class Program
+    {
+        public struct Book
+        {
+            public decimal price;
+            public string author;
+            public string title;
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            Book book;
+            book.author = "Pythagoras Danso";
+            book.title = "My love for you";
+            book.price = 90.24m;
+
+            Console.WriteLine(book.ToString());
+
+        }
+    }
+}
